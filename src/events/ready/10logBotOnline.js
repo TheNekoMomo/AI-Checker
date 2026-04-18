4451391bc6c5dc7148e75cf81f27880926a1b589
Momo <moharryg@gmail.com>
2026-04-19 00:28:23 +0100

    log servers

--- src/events/ready/10logBotOnline.js
+++ src/events/ready/10logBotOnline.js
@@ -5,4 +5,8 @@
 /** @type {EventFileHandler} */
 module.exports = (client) => {
     console.log(`${client.user?.tag} is online.`);
+
+    client.guilds.cache.forEach(guild => {
+        console.log(`Server Name: ${guild.name}, ${guild.memberCount} members`);
+    });
 };
